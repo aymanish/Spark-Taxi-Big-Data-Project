a0deb6eb47c900a755564f3affb8bbfc433bc184
aymanish <az804@york.ac.uk>
2024-09-20 12:05:45 +0100

    completed ColumnsAndExpressions exercises

--- spark-essentials-start/src/main/scala/part2dataframes/ColumnsAndExpressions.scala
+++ spark-essentials-start/src/main/scala/part2dataframes/ColumnsAndExpressions.scala
@@ -81,29 +81,30 @@ object ColumnsAndExpressions extends App {
   val carsWithKg3DF =carsDF.withColumn("Weight_in_kg_3", col("Weight_in_lbs") / 2.2)
 
   // Renaming a column
-  val carsWithColRenamed = carsDF.withColumnRenamed("Weight_in_lbs", "Weight in pounds")
+  //val carsWithColRenamed = carsDF.withColumnRenamed("Weight_in_lbs", "Weight in pounds")
   // careful with column names -> above we renames without _ for better parsing
   // so when we use that col name use back tick (`) as shown below
-  carsWithColRenamed.selectExpr(
-    "Weight in pounds`"
-  )
+  //carsWithColRenamed.selectExpr(
+  //  "Weight in pounds`"
+  //)
 
   // Removing a column:
-  carsWithColRenamed.drop("Cylinders", "Displacement")
+  //carsWithColRenamed.drop("Cylinders", "Displacement")
 
+  /*
   // Filtering: use filter() or where()
   val europeanCarsDF = carsDF.filter(col("Origin") =!= "USA") // filter by column origin not equal to USA
   val europeanCarsDF2 = carsDF.where(col("Origin") =!= "USA")
 
   // filtering with expression strings
-  val americanCarsDF = carsDF.filter("Origin = USA")
+  val americanCarsDF = carsDF.filter("Origin === USA")
 
   //chaining filters
   val americanPowerfulCarsDF = carsDF.filter(col("Origin") === "USA").filter(col("Horsepower") > 150)
   // OR
   val americanPowerfulCarsDF2 = carsDF.filter(col("Origin") === "USA" and col("Horsepower") > 150)
   // OR
-  val americanPowerfulCarsDF3 = carsDF.filter("Origin = 'USA' and Horsepower > 150")
+  val americanPowerfulCarsDF3 = carsDF.filter("Origin === USA and Horsepower > 150")
 
   // unioning = adding more rows
   val moreCarsDF = spark.read
@@ -115,5 +116,63 @@ object ColumnsAndExpressions extends App {
   // disting values: shows all unique countries
   val allCountriesDF = carsDF.select("Origin").distinct()
   allCountriesDF.show()
+
+   */
+  // Exercises:
+
+
+  // 1)  read the movies df and select 2 columns of your choice
+  val moviesDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+  val selectedMovieColsDF = moviesDF.select(
+    moviesDF.col("Title"),
+    col("Major_Genre"),
+    $"Worldwide_Gross",
+    expr("US_Gross")
+  )
+
+  selectedMovieColsDF.show()
+
+  // 2) create another column summing up the total profit of movies
+  //    US_Gross + Worldwide_Gross + DVD_Sales
+  val moviesTotalProfitDF1 = moviesDF.selectExpr(
+    "US_Gross + Worldwide_Gross"
+  )
+  val moviesTotalProfitDF2 = moviesDF.select(
+    col("Title"),
+    expr("US_Gross + Worldwide_Gross").as("Total_Profit")
+  )
+
+  val moviesTotalProfitDF3 = moviesDF.select(
+    col("Title"),
+    col("US_Gross"),
+    col("Worldwide_Gross"),
+    col("US_DVD_Sales"),
+    (col("US_Gross") + col("Worldwide_Gross")).as("Total_Profit")
+  )
+
+  moviesTotalProfitDF1.show()
+  moviesTotalProfitDF2.show()
+  moviesTotalProfitDF3.show()
+
+
+  // 3) Select all the comedy movies from the Major_Genre column
+  //    that have IMDB_Rating above 6
+  // use as many ways as possible
+  val moviesGoodComedyDF1 = moviesDF
+    .filter(col("Major_Genre") === "Comedy")
+    .filter(col("IMDB_Rating") > 6)
+  val moviesGoodComedyDF2 = moviesDF.filter(col("Major_Genre") === "Comedy" and col("IMDB_Rating") > 6)
+  val moviesGoodComedyDF3 = moviesDF.filter("Major_Genre = 'Comedy' and  IMDB_Rating > 6")
+
+  moviesGoodComedyDF1.show()
+  moviesGoodComedyDF2.show()
+  moviesGoodComedyDF3.show()
+  
+  // alternatively
+  val goodcomedy = moviesDF.select("Title", "IMDB_Rating")
+    .where("Major_Genre = 'Comedy' and IMDB_Rating > 6")
 
 }
